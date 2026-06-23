a17e458123f9eae1874250cbfa2bf30719f8d8ab
SubinAdhikari <53433328+SubinAdhikari@users.noreply.github.com>
2026-06-23 08:31:45 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        var left = 0;
+        var right = numbers.length - 1;
+        while (left < right){
+            if(numbers[left]+numbers[right] == target){
+                return [left+1,right+1];
+            }else if(numbers[left]+numbers[right] > target){
+                right--;
+            }else{
+                left++;
+            }
+        }
+    }
+}
